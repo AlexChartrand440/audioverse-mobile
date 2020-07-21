b2a4b5f85da12444dd8e2ae2f97cf1423b53fd3a
Matthew Leffler <mattleff@gmail.com>
2020-07-21 12:41:58 -0600

    Format .js in root dir

--- index.js
+++ index.js
@@ -2,32 +2,28 @@
  * @format
  */
 
-import React from 'react'
-import {
-  AppRegistry,
-  Text,
-  Platform,
-} from 'react-native'
-import TrackPlayer from 'react-native-track-player'
+import React from 'react';
+import { AppRegistry, Platform, Text } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 
-import configureStore from './src/store/configureStore'
-import createApp from './App'
-import createEventHandler from './src/utils/event-handler'
+import createApp from './App';
+import configureStore from './src/store/configureStore';
+import createEventHandler from './src/utils/event-handler';
 
-const { store, persistor } = configureStore()
+const { store, persistor } = configureStore();
 
-AppRegistry.registerComponent('AudioVerse', () => createApp(store, persistor))
-TrackPlayer.registerPlaybackService(() => createEventHandler(store))
+AppRegistry.registerComponent('AudioVerse', () => createApp(store, persistor));
+TrackPlayer.registerPlaybackService(() => createEventHandler(store));
 
 // On android, OnePlus and Oppo devices use a custom font that may cause truncated text on some parts,
 // as a workaround set the default Text font to Roboto or Arial
 // https://github.com/facebook/react-native/issues/15114
 if (Platform.OS === 'android') {
-  const oldRender = Text.render
-  Text.render = function (...args) {
-      const origin = oldRender.call(this, ...args)
-      return React.cloneElement(origin, {
-        style: [{fontFamily: 'Roboto'}, origin.props.style],
-      })
-  }
+	const oldRender = Text.render;
+	Text.render = function (...args) {
+		const origin = oldRender.call(this, ...args);
+		return React.cloneElement(origin, {
+			style: [{ fontFamily: 'Roboto' }, origin.props.style],
+		});
+	};
 }
